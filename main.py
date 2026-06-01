ea63af9abdf5e712a9811af46ce4450e52046da4
Vitor R Marques <dleggdraw@gmail.com>
2026-06-01 15:08:36 -0300

    Arrumando algoritmos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import time
 
 # =====================================================
 # GRAFO
@@ -23,7 +24,7 @@ comodos = [
 G.add_nodes_from(comodos)
 
 # =====================================================
-# LUZES
+# ESTADO DAS LUZES
 # =====================================================
 
 estado_luzes = {
@@ -33,6 +34,10 @@ estado_luzes = {
     "Banheiro": False,
     "Lavanderia": False
 }
+
+# =====================================================
+# POTÊNCIA DAS LUZES
+# =====================================================
 
 potencia_luzes = {
     "Sala": 150,
@@ -107,7 +112,9 @@ G.add_edge("QD", "Cozinha", distancia=10)
 G.add_edge("QD", "Banheiro", distancia=4)
 G.add_edge("QD", "Lavanderia", distancia=8)
 
-# periféricos
+# =====================================================
+# CONECTANDO PERIFÉRICOS
+# =====================================================
 
 for nome, dados in perifericos.items():
 
@@ -120,62 +127,51 @@ for nome, dados in perifericos.items():
     )
 
 # =====================================================
-# BFS
-# =====================================================
-
-def bfs_ligar(comodo):
-
-    estado_luzes[comodo] = True
-
-def bfs_desligar(comodo):
-
-    estado_luzes[comodo] = False
-
-# =====================================================
-# PERIFÉRICOS
-# =====================================================
-
-def ligar_periferico(nome):
-
-    perifericos[nome]["ligado"] = True
-
-def desligar_periferico(nome):
-
-    perifericos[nome]["ligado"] = False
-
-# =====================================================
 # CONSUMO
 # =====================================================
+
+energia_total = 0
+ultimo_tempo = time.time()
 
 def calcular_consumo():
 
     total = 0
 
-    # luzes
-
     for comodo, ligado in estado_luzes.items():
 
         if ligado:
-            total += potencia_luzes[comodo]
 
-    # periféricos
+            total += potencia_luzes[comodo]
 
     for nome, dados in perifericos.items():
 
         if dados["ligado"]:
+
             total += dados["watts"]
 
     return total
 
-energia_total = 0
+# =====================================================
+# MONITORAMENTO
+# =====================================================
+
 def mostrar_consumo():
 
     global energia_total
+    global ultimo_tempo
+
+    agora = time.time()
+
+    delta = agora - ultimo_tempo
+
+    ultimo_tempo = agora
 
     consumo = calcular_consumo()
 
-    # atualização por segundo
-    energia_total += consumo / 3600
+    # energia em Wh
+    energia_total += (
+        consumo * delta
+    ) / 3600
 
     print("\n================================")
     print(" MONITORAMENTO ENERGÉTICO ")
@@ -195,15 +191,19 @@ def mostrar_consumo():
 
     print("\n--------------------------------")
 
-    print("Luzes:")
+    print("LUZES:")
 
     for comodo, ligado in estado_luzes.items():
 
-        estado = "LIGADA" if ligado else "DESLIGADA"
+        estado = (
+            "LIGADA"
+            if ligado
+            else "DESLIGADA"
+        )
 
         print(f"{comodo}: {estado}")
 
-    print("\nPeriféricos:")
+    print("\nPERIFÉRICOS:")
 
     for nome, dados in perifericos.items():
 
@@ -222,6 +222,162 @@ def mostrar_consumo():
     print("================================\n")
 
 # =====================================================
+# BFS
+# =====================================================
+
+def bfs_energia(inicio, ligar=True, bloqueados=None):
+
+    if bloqueados is None:
+
+        bloqueados = []
+
+    fila = [inicio]
+
+    visitados = set()
+
+    print("\n============ BFS ============\n")
+
+    while fila:
+
+        atual = fila.pop(0)
+
+        # -----------------------------------------
+
+        if atual in bloqueados:
+
+            print(f"{atual}: BLOQUEADO")
+            continue
+
+        # -----------------------------------------
+
+        if atual not in visitados:
+
+            visitados.add(atual)
+
+            # -------------------------------------
+            # CÔMODOS
+            # -------------------------------------
+
+            if atual in estado_luzes:
+
+                estado_luzes[atual] = ligar
+
+                estado = (
+                    "LIGADA"
+                    if ligar
+                    else "DESLIGADA"
+                )
+
+                print(f"{atual}: {estado}")
+
+            # -------------------------------------
+            # PERIFÉRICOS
+            # -------------------------------------
+
+            if atual in perifericos:
+
+                perifericos[atual]["ligado"] = ligar
+
+                estado = (
+                    "LIGADO"
+                    if ligar
+                    else "DESLIGADO"
+                )
+
+                print(f"{atual}: {estado}")
+
+            # -------------------------------------
+            # VIZINHOS
+            # -------------------------------------
+
+            for vizinho in G.neighbors(atual):
+
+                # impede voltar ao quadro
+
+                if vizinho == "QD":
+
+                    continue
+
+                if (
+                    vizinho not in visitados
+                    and vizinho not in bloqueados
+                ):
+
+                    fila.append(vizinho)
+
+    print("\n=============================\n")
+
+# =====================================================
+# DFS
+# =====================================================
+
+def dfs_energia(inicio):
+
+    visitados = set()
+
+    print("\n============ DFS ============\n")
+
+    def dfs(no):
+
+        visitados.add(no)
+
+        print(f"\nVisitando: {no}")
+
+        # -----------------------------------------
+        # CÔMODOS
+        # -----------------------------------------
+
+        if no in estado_luzes:
+
+            estado = (
+                "LIGADA"
+                if estado_luzes[no]
+                else "DESLIGADA"
+            )
+
+            print("Tipo: Cômodo")
+            print(f"Estado: {estado}")
+
+            print(
+                f"Potência Luz: "
+                f"{potencia_luzes[no]}W"
+            )
+
+        # -----------------------------------------
+        # PERIFÉRICOS
+        # -----------------------------------------
+
+        elif no in perifericos:
+
+            estado = (
+                "LIGADO"
+                if perifericos[no]["ligado"]
+                else "DESLIGADO"
+            )
+
+            print("Tipo: Periférico")
+            print(f"Estado: {estado}")
+
+            print(
+                f"Potência: "
+                f"{perifericos[no]['watts']}W"
+            )
+
+        # -----------------------------------------
+
+        for vizinho in G.neighbors(no):
+
+            if vizinho not in visitados:
+
+                dfs(vizinho)
+
+    dfs(inicio)
+
+    print("\n=============================\n")
+
+    return visitados
+
+# =====================================================
 # DESENHO
 # =====================================================
 
@@ -231,7 +387,9 @@ fig = plt.figure(figsize=(14, 10))
 
 def desenhar_grafo():
 
-    plt.clf()
+    fig.clear()
+
+    ax = fig.add_subplot(111)
 
     pos = {
 
@@ -265,47 +423,79 @@ def desenhar_grafo():
 
     for no in G.nodes():
 
+        # quadro
+
         if no == "QD":
 
             cores.append("orange")
 
+        # cômodos
+
         elif no in estado_luzes:
 
             if estado_luzes[no]:
-                cores.append("yellow")
+
+                cores.append("gold")
+
             else:
-                cores.append("lightblue")
+
+                cores.append("deepskyblue")
+
+        # periféricos
 
         elif no in perifericos:
 
             if perifericos[no]["ligado"]:
+
                 cores.append("red")
+
             else:
-                cores.append("gray")
+
+                cores.append("dimgray")
+
+        else:
+
+            cores.append("white")
+
+    # =================================================
+    # DESENHO
+    # =================================================
 
     nx.draw(
         G,
         pos,
+        ax=ax,
         with_labels=True,
         node_color=cores,
         node_size=2500,
         font_size=9
     )
 
+    # =================================================
+    # DISTÂNCIAS
+    # =================================================
+
     edge_labels = {}
 
     for u, v, data in G.edges(data=True):
 
-        edge_labels[(u, v)] = f"{data['distancia']}m"
+        edge_labels[(u, v)] = (
+            f"{data['distancia']}m"
+        )
 
     nx.draw_networkx_edge_labels(
         G,
         pos,
-        edge_labels=edge_labels
+        edge_labels=edge_labels,
+        ax=ax
     )
 
+    # =================================================
+
     plt.title(
-        f"Consumo Atual: {calcular_consumo()} W"
+        f"Sistema Elétrico Residencial\n"
+        f"Consumo Atual: "
+        f"{calcular_consumo()} W"
     )
 
     plt.axis("off")
@@ -314,7 +504,40 @@ def desenhar_grafo():
     plt.pause(0.1)
 
 # =====================================================
-# MENU DOS CÔMODOS
+# MENU BFS
+# =====================================================
+
+def menu_bfs(comodo):
+
+    acao = input(
+        "\n1 -> Ligar\n"
+        "2 -> Desligar\n"
+        "Escolha: "
+    )
+
+    if acao == "1":
+
+        ligar = True
+
+    elif acao == "2":
+
+        ligar = False
+
+    else:
+
+        print("Opção inválida")
+        return
+
+    bfs_energia(
+        comodo,
+        ligar
+    )
+
+    desenhar_grafo()
+    mostrar_consumo()
+
+# =====================================================
+# MENUS DOS CÔMODOS
 # =====================================================
 
 def menu_sala():
@@ -329,23 +552,28 @@ def menu_sala():
         print("3 -> Ligar TV")
         print("4 -> Desligar TV")
 
-        print("0 -> Sair da Sala")
+        print("0 -> Voltar")
 
         op = input("Escolha: ")
 
         if op == "1":
-            bfs_ligar("Sala")
+
+            estado_luzes["Sala"] = True
 
         elif op == "2":
-            bfs_desligar("Sala")
+
+            estado_luzes["Sala"] = False
 
         elif op == "3":
-            ligar_periferico("TV_Sala")
+
+            perifericos["TV_Sala"]["ligado"] = True
 
         elif op == "4":
-            desligar_periferico("TV_Sala")
+
+            perifericos["TV_Sala"]["ligado"] = False
 
         elif op == "0":
+
             break
 
         desenhar_grafo()
@@ -368,29 +596,36 @@ def menu_quarto():
         print("5 -> Ligar Computador")
         print("6 -> Desligar Computador")
 
-        print("0 -> Sair do Quarto")
+        print("0 -> Voltar")
 
         op = input("Escolha: ")
 
         if op == "1":
-            bfs_ligar("Quarto")
+
+            estado_luzes["Quarto"] = True
 
         elif op == "2":
-            bfs_desligar("Quarto")
+
+            estado_luzes["Quarto"] = False
 
         elif op == "3":
-            ligar_periferico("TV_Quarto")
+
+            perifericos["TV_Quarto"]["ligado"] = True
 
         elif op == "4":
-            desligar_periferico("TV_Quarto")
+
+            perifericos["TV_Quarto"]["ligado"] = False
 
         elif op == "5":
-            ligar_periferico("Computador")
+
+            perifericos["Computador"]["ligado"] = True
 
         elif op == "6":
-            desligar_periferico("Computador")
+
+            perifericos["Computador"]["ligado"] = False
 
         elif op == "0":
+
             break
 
         desenhar_grafo()
@@ -408,74 +643,184 @@ def menu_cozinha():
         print("2 -> Desligar Luz")
 
         print("3 -> Ligar Geladeira")
-        print("4 -> Ligar Fogão")
+        print("4 -> Desligar Geladeira")
 
-        print("5 -> Ligar Liquidificador")
+        print("5 -> Ligar Fogao")
+        print("6 -> Desligar Fogao")
 
-        print("6 -> Desligar Geladeira")
-        print("7 -> Desligar Fogão")
-
+        print("7 -> Ligar Liquidificador")
         print("8 -> Desligar Liquidificador")
 
-        print("0 -> Sair da Cozinha")
+        print("0 -> Voltar")
 
         op = input("Escolha: ")
 
         if op == "1":
-            bfs_ligar("Cozinha")
+
+            estado_luzes["Cozinha"] = True
 
         elif op == "2":
-            bfs_desligar("Cozinha")
+
+            estado_luzes["Cozinha"] = False
 
         elif op == "3":
-            ligar_periferico("Geladeira")
+
+            perifericos["Geladeira"]["ligado"] = True
 
         elif op == "4":
-            ligar_periferico("Fogao")
+
+            perifericos["Geladeira"]["ligado"] = False
 
         elif op == "5":
-            ligar_periferico("Liquidificador")
+
+            perifericos["Fogao"]["ligado"] = True
 
         elif op == "6":
-            desligar_periferico("Geladeira")
+
+            perifericos["Fogao"]["ligado"] = False
 
         elif op == "7":
-            desligar_periferico("Fogao")
+
+            perifericos["Liquidificador"]["ligado"] = True
 
         elif op == "8":
-            desligar_periferico("Liquidificador")
+
+            perifericos["Liquidificador"]["ligado"] = False
 
         elif op == "0":
+
             break
 
         desenhar_grafo()
         mostrar_consumo()
 
+# =====================================================
+
 def menu_banheiro():
+
     while True:
+
         print("\n===== BANHEIRO =====")
+
         print("1 -> Ligar Luz")
         print("2 -> Desligar Luz")
+
         print("3 -> Ligar Chuveiro")
-        print("0 -> Sair do Banheiro")
+        print("4 -> Desligar Chuveiro")
+
+        print("0 -> Voltar")
 
         op = input("Escolha: ")
 
         if op == "1":
-            bfs_ligar("Banheiro")
+
+            estado_luzes["Banheiro"] = True
 
         elif op == "2":
-            bfs_desligar("Banheiro")
+
+            estado_luzes["Banheiro"] = False
 
         elif op == "3":
-            ligar_periferico("Chuveiro")
+
+            perifericos["Chuveiro"]["ligado"] = True
+
+        elif op == "4":
+
+            perifericos["Chuveiro"]["ligado"] = False
 
         elif op == "0":
+
             break
 
         desenhar_grafo()
         mostrar_consumo()
 
+# =====================================================
+
+def menu_lavanderia():
+
+    while True:
+
+        print("\n===== LAVANDERIA =====")
+
+        print("1 -> Ligar Luz")
+        print("2 -> Desligar Luz")
+
+        print("3 -> Ligar Máquina")
+        print("4 -> Desligar Máquina")
+
+        print("0 -> Voltar")
+
+        op = input("Escolha: ")
+
+        if op == "1":
+
+            estado_luzes["Lavanderia"] = True
+
+        elif op == "2":
+
+            estado_luzes["Lavanderia"] = False
+
+        elif op == "3":
+
+            perifericos["Maquina_Lavar"]["ligado"] = True
+
+        elif op == "4":
+
+            perifericos["Maquina_Lavar"]["ligado"] = False
+
+        elif op == "0":
+
+            break
+
+        desenhar_grafo()
+        mostrar_consumo()
+
+# =====================================================
+# MENU CÔMODOS
+# =====================================================
+
+def menu_comodos():
+
+    while True:
+
+        print("\n==============================")
+        print(" MENU DOS CÔMODOS ")
+        print("==============================")
+
+        print("\n1 -> Sala")
+        print("2 -> Quarto")
+        print("3 -> Cozinha")
+        print("4 -> Banheiro")
+        print("5 -> Lavanderia")
+
+        print("0 -> Voltar")
+
+        op = input("Escolha: ")
+
+        if op == "1":
+
+            menu_sala()
+
+        elif op == "2":
+
+            menu_quarto()
+
+        elif op == "3":
+
+            menu_cozinha()
+
+        elif op == "4":
+
+            menu_banheiro()
+
+        elif op == "5":
+
+            menu_lavanderia()
+
+        elif op == "0":
+
+            break
 
 # =====================================================
 # MENU PRINCIPAL
@@ -489,28 +834,70 @@ while True:
     print(" SISTEMA ELÉTRICO ")
     print("==============================")
 
-    print("\n1 -> Sala")
-    print("2 -> Quarto")
-    print("3 -> Cozinha")
-    print("4 -> Banheiro")
+    print("\n1 -> Cômodos")
+    print("2 -> Menu BFS (Ligar tudo em uma sala)")
+    print("3 -> Verificação DFS (Verificacao Ligados ou Desligados)")
 
     print("0 -> Sair")
 
-    entrada = input("Escolha: ")
+    entrada = input("\nEscolha: ")
+
+    # =================================================
+    # MENU CÔMODOS
+    # =================================================
 
     if entrada == "1":
-        menu_sala()
+
+        menu_comodos()
+
+    # =================================================
+    # MENU BFS
+    # =================================================
 
     elif entrada == "2":
-        menu_quarto()
+
+        print("\n===== MENU BFS =====")
+
+        print("1 -> Sala")
+        print("2 -> Quarto")
+        print("3 -> Cozinha")
+        print("4 -> Banheiro")
+        print("5 -> Lavanderia")
+
+        op = input("Escolha: ")
+
+        if op == "1":
+
+            menu_bfs("Sala")
+
+        elif op == "2":
+
+            menu_bfs("Quarto")
+
+        elif op == "3":
+
+            menu_bfs("Cozinha")
+
+        elif op == "4":
+
+            menu_bfs("Banheiro")
+
+        elif op == "5":
+
+            menu_bfs("Lavanderia")
+
+    # =================================================
+    # DFS
+    # =================================================
 
     elif entrada == "3":
-        menu_cozinha()
 
-    elif entrada == "4":
-        menu_banheiro()
+        dfs_energia("QD")
+
+    # =================================================
 
     elif entrada == "0":
+
         break
 
     desenhar_grafo()
